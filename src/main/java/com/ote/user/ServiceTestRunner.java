4d595173bf80417e5be074cf8359904fe157284d
OTerrien <oterrien@neuf.fr>
2017-12-27 23:57:30 +0100

    add postgres

--- src/main/java/com/ote/user/ServiceTestRunner.java
+++ src/main/java/com/ote/user/ServiceTestRunner.java
@@ -1,11 +1,19 @@
 package com.ote.user;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.math.BigInteger;
+
 @SpringBootApplication
+@Slf4j
 public class ServiceTestRunner {
 
     public static void main(String[] args) throws Throwable {
@@ -14,7 +22,26 @@ public class ServiceTestRunner {
 
     @Bean
     public RestTemplate restTemplate() {
-       return new RestTemplate();
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+
+        return args -> {
+            EntityManager session = entityManagerFactory.createEntityManager();
+            try {
+                BigInteger id = (BigInteger) session.createNativeQuery("select max(id) from my_table").getSingleResult();
+                log.info("##################### : " + id);
+            } finally {
+                if (session.isOpen()) {
+                    session.close();
+                }
+            }
+        };
     }
 
 }
