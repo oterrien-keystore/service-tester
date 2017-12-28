a8520743e7acd5b6c90d81287f88f315315f80a1
OTerrien <oterrien@neuf.fr>
2017-12-28 11:04:30 +0100

    move configuration & remove database test

--- src/main/java/com/ote/user/ServiceTestRunner.java
+++ src/main/java/com/ote/user/ServiceTestRunner.java
@@ -1,22 +1,16 @@
 package com.ote.user;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import java.math.BigInteger;
-
 @SpringBootApplication
 @Slf4j
 public class ServiceTestRunner {
 
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
         SpringApplication.run(ServiceTestRunner.class, args);
     }
 
@@ -24,24 +18,4 @@ public class ServiceTestRunner {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-
-        return args -> {
-            EntityManager session = entityManagerFactory.createEntityManager();
-            try {
-                BigInteger id = (BigInteger) session.createNativeQuery("select max(id) from my_table").getSingleResult();
-                log.info("##################### : " + id);
-            } finally {
-                if (session.isOpen()) {
-                    session.close();
-                }
-            }
-        };
-    }
-
 }
